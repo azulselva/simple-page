44d83e09f6b5e5c5785f1f2f20ecf254a46fabee
mazsan <mazsan@gmail.com>
2022-08-17 12:07:38 +0200

    agrega columnas centrales finales

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function alertMessage() {
+  window.alert("Hola mundo")
+}
+
+/* window.alert('¿Puede soportar toda la belleza que encontarrá aquí?'); */
+const buttonModern = document.getElementById("buttonModern")
+console.dir(buttonModern)
+
+buttonModern.onclick = alertMessage
